4ed90cc1ef7068adc0a53b5e94c0368e62dc1123
dwikybp2 <dwikybagusprasetyo@gmail.com>
2024-09-18 22:54:33 +0700

    init

---
+++ src/ui/DeleteItem.jsx
@@ -0,0 +1,11 @@
+import { useDispatch } from "react-redux";
+import Button from "./Button";
+import { deleteItem } from "../features/cart/cartSlice";
+
+function DeleteItem({pizzaId}) {
+    const dispatch = useDispatch();
+
+    return <Button type="small" onClick={() => dispatch(deleteItem(pizzaId))}>Delete</Button>;
+}
+
+export default DeleteItem
